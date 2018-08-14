fbf57b79434330c870629fdf06b6d512cc9e28d1
afiefsky <afiefsky@gmail.com>
2018-08-14 23:55:00 +0700

    fixed bug on qty at cart

--- application/models/Cart_detail_model.php
+++ application/models/Cart_detail_model.php
@@ -4,7 +4,7 @@ class Cart_detail_model extends CI_Model
 {
     public function get_with_product($cart_id)
     {
-        $this->db->select('*, crd.id AS detail_id');
+        $this->db->select('*, crd.id AS detail_id, crd.qty AS qty');
         $this->db->from('carts_details AS crd');
         $this->db->join('products AS prd', 'prd.id = crd.product_id', 'right');
         $this->db->where('cart_id', $cart_id);
