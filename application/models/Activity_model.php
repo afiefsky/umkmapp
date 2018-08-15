93d6dc2cc631f9b0ce058f5b260a850566263d3c
afiefsky <afiefsky@gmail.com>
2018-08-15 19:14:15 +0700

    testing phase 1

--- application/models/Activity_model.php
+++ application/models/Activity_model.php
@@ -10,7 +10,7 @@ class Activity_model extends CI_Model
                         ON act.company_id = com.id
                     INNER JOIN users_companies AS usc
                         ON usc.company_id = com.id
-                    WHERE usc.user_id = 2";
+                    WHERE usc.user_id = $user_id";
         return $this->db->query($query);
     }
 }
