ab4fed859b7168a1d65f47c94faf4a64f441fa4e
afiefsky <afiefsky@gmail.com>
2018-08-20 09:37:51 +0700

    Fixing bug on check_session_admin() on app_helper

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -14,7 +14,7 @@ function check_session_admin()
 {
     $CI = & get_instance();
     $session = $CI->session->userdata('username');
-    if ($session!='admin' || $session!='superadmin' || $session=='')
+    if ($session!='admin' && $session!='superadmin' && $session=='')
     {
         redirect('auth');
     }
