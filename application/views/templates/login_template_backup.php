25668e873a4689e3022976a8348b696843f29ce5
Muhammad Afief Farista <afiefsky@gmail.com>
2019-05-15 21:47:10 +0000

    Apply fixes from StyleCI

--- application/views/templates/login_template_backup.php
+++ application/views/templates/login_template_backup.php
@@ -38,6 +38,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
   </body>
 </html>
